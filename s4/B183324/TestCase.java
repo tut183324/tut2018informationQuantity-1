d6d9006accfa3bf9172299818e24577dfef4b08f
Yudai Kubota <yudaikubota.nature@gmail.com>
2018-12-15 13:12:04 +0900

    TestCase追加?

--- s4/B183324/TestCase.java
+++ s4/B183324/TestCase.java
@@ -1,4 +1,4 @@
-package s4.B183324; // Please modify to s4.Bnnnnnn, where nnnnnn is your student ID. 
+package s4.B183324; // Please modify to s4.Bnnnnnn, where nnnnnn is your student ID.
 import java.lang.*;
 import s4.specification.*;
 
@@ -24,8 +24,8 @@ public interface InformationEstimatorInterface{
 // It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
 // The behavior is undefined, if the true value is finete but larger than Double.MAX_VALUE.
 // Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
-// Otherwise, estimation of information quantity, 
-}                        
+// Otherwise, estimation of information quantity,
+}
 */
 
 
@@ -70,5 +70,4 @@ public class TestCase {
 	}
 
     }
-}	    
-	    
+}
